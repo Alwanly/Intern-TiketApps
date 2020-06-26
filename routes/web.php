735c3ce4bf12de5b30ef62d6e975e763e8daa686
Alwan Alyafi Mulyawan <alwanalyafi7@gmail.com>
2020-06-26 16:35:46 +0700

    solve transactio

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,17 @@ Route::get('/dashboard',"Admin\AdminController@index")->name('dashboard');
 Route::get('/packet',"Admin\AdminController@packet")->name('packetList');
 Route::get('/packet/detail/{id}',"Admin\AdminController@detailPacket")->name('packetDetail');
 Route::get('/packet/create',"Admin\AdminController@createPacket")->name('packetCreate');
+
 Route::get('/agents',"Admin\AdminController@indexAgent")->name('agentList');
 Route::get('/agents/detail/{id}','Admin\AdminController@detailAgent')->name('agentDetail');
+
 Route::get('/payment','Admin\PaymentController@index')->name('paymentList');
 Route::get('/payment/detail/{id}','Admin\PaymentController@detail')->name('paymentDetail');
+
 Route::get('/transaction','Admin\TransactionController@index')->name('transactionList');
 Route::get('/transaction/detail/{id}','Admin\TransactionController@detail')->name('transactionDetail');
 Route::get('/transaction/update','Admin\TransactionController@update')->name('transactionUpdate');
+
 Route::get('/master_data','Admin\MasterDataController@index')->name('masterData');
 Route::get('/master_data/create_method','Admin\MasterDataController@createMethod')->name('createMethod');
 Route::get('/master_data/create_room_type','Admin\MasterDataController@createRoomType')->name('createRoom');
