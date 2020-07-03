3e88f3d15a6f37dc231bc2f7893d0d49c75bb8e3
Alwan Alyafi Mulyawan <alwanalyafi7@gmail.com>
2020-07-03 15:04:14 +0700

    shut down admin

--- resources/views/layouts/appUser.blade.php
+++ resources/views/layouts/appUser.blade.php
@@ -21,70 +21,7 @@
 </head>
 <body class="">
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light shadow-sm nav-bg">
-        <div class="container">
-            <a class="navbar-brand text-bold " href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-                </ul>
-
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto text-center">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item {{(request()->is('home')) ? 'active' : ''}}">
-                            <a class="nav-link" href="/home">
-                                <i class="nav-icon fas fa-home"></i><br>Home
-                            </a>
-                        </li>
-                        <li class="nav-item {{(request()->is('purchase/list')) ? 'active' : ''}} ">
-                            <a class="nav-link " href="/purchase/list">
-                                <i class="nav-icon fas fa-credit-card"></i><br>Transaction
-                            </a>
-                        </li>
-                        <li class="nav-item {{(request()->is('account/profile') ? 'active' : '')}} ">
-                            <a class="nav-link " href="/account/profile">
-                                <i class="nav-icon fas fa-user-circle"></i><br>Accounts
-                            </a>
-                        </li>
-{{--                        <li class="nav-item">--}}
-{{--                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>--}}
-{{--                        </li>--}}
-{{--                        @if (Route::has('register'))--}}
-{{--                            <li class="nav-item">--}}
-{{--                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>--}}
-{{--                            </li>--}}
-{{--                        @endif--}}
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
+    @include('layouts.topnavUser')
     <main class="py-4">
         @yield('content')
     </main>
