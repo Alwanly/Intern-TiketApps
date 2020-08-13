63e6ef8c23cd461f17ce5f14c67579aa4c53fd0a
Alwan Alyafi Mulyawan <alwanalyafi7@gmail.com>
2020-08-13 14:40:14 +0700

    seeder update

--- database/seeds/StatusMasterTableSeeder.php
+++ database/seeds/StatusMasterTableSeeder.php
@@ -13,11 +13,11 @@ class StatusMasterTableSeeder extends Seeder
     public function run()
     {
         DB::table('status_masters')->delete();
-        $status_name =['Active','inActive','Menunggu Pembayaran','Pembayaran Berhasil','Menunggu Waktu Manasik',
+        $status_name =['','','Active','inActive','Menunggu Pembayaran','Pembayaran Berhasil','Menunggu Waktu Manasik',
             'Menunggu Waktu Berangkat','Dalam Perjalanan Umroh','Tiba di Tanah Air','Belum Dibayar','Menunggu Konfirmasi',
             'Sudah Dibayar','Pembayaran Ditolak','Pembayaran Gagal','Pembayaran Expired','Ditolak','Menunggu Verifikasi'];
-        $status_code =['sa','sa','st','st','st','st','st','st','sp','sp','sp','sp','sp','sp','sag','sag'];
-        for ($i = 2;$i<= 17 ;$i++){
+        $status_code =['','','sa','sa','st','st','st','st','st','st','sp','sp','sp','sp','sp','sp','sag','sag'];
+        for ($i = 0;$i<= count($status_name) ;$i++){
             DB::table('status_masters')->insert([
                 'id'=>$i,
                 'status_code'=>$status_code[$i],
